6a2d4e3fada5ba0d48272e25fac8dc93703bb0d9
darko-boskovski <darko_b23@hotmail.com>
2021-05-17 22:08:48 +0200

    Update Program.cs

--- C-Sharp Advanced Workshop Exercises/Exercise 4/Program.cs
+++ C-Sharp Advanced Workshop Exercises/Exercise 4/Program.cs
@@ -8,28 +8,28 @@ namespace Exercise_4
         public delegate bool TwoStringDelegate(string word1, string word2);
         static void Main(string[] args)
         {
-            
+
             Func<string, string, bool> compareString = (word1, word2) => word1.Length > word2.Length;
             Func<string, string, bool> sameCharacterStart = (word1, word2) => word1[0] == word2[0];
-            Func<string, string, bool> sameCharacterEnd = (word1, word2) => word1[word1.Length -1] == word2[word2.Length-1];
+            Func<string, string, bool> sameCharacterEnd = (word1, word2) => word1.Substring(word1.Length - 1) == word2.Substring(word2.Length - 1);
 
             TwoStringDelegate compareStringDelegate = new TwoStringDelegate(compareString);
-            TwoStringDelegate startsOnSameDelegate = new TwoStringDelegate(compareString);
-            TwoStringDelegate endsOnSameDelegate = new TwoStringDelegate(compareString);
+            TwoStringDelegate startsOnSameDelegate = new TwoStringDelegate(sameCharacterStart);
+            TwoStringDelegate endsOnSameDelegate = new TwoStringDelegate(sameCharacterEnd);
 
-            if(StringMagic("Pero", "Blazo", compareStringDelegate)) { Console.WriteLine($"The First word is longer then the second"); };  
-             Console.WriteLine($"The Second word is longer then the first");
-            Console.WriteLine("-------------------------------------------");
-          
-
-
-            if(StringMagic("Pero", "Blazo", startsOnSameDelegate)) { Console.WriteLine($"The Words Start on the Same Letter"); };
-            { Console.WriteLine($"The Words Start on  diferent Letters"); }
+            if (StringMagic("Pero", "Blazo", compareStringDelegate)) { Console.WriteLine($"The First word is longer then the second"); }
+            else { Console.WriteLine($"The Second word is longer then the first"); }
             Console.WriteLine("-------------------------------------------");
 
 
-            if (StringMagic("Pero", "Blazo", endsOnSameDelegate)) { Console.WriteLine($"The Words End on the Same Letter"); };
-            { Console.WriteLine($"The Words End on  diferent Letters"); }
+
+            if (StringMagic("Pero", "Blazo", startsOnSameDelegate)) { Console.WriteLine($"The Words Start on the Same Letter"); }
+            else { Console.WriteLine($"The Words Start on diferent Letters"); }
+            Console.WriteLine("-------------------------------------------");
+
+
+            if (StringMagic("Pero", "Blazo", endsOnSameDelegate)) { Console.WriteLine($"The Words End on the Same Letter"); }
+            else { Console.WriteLine($"The Words End on  diferent Letters"); };
             Console.WriteLine("-------------------------------------------");
 
 
@@ -39,12 +39,12 @@ namespace Exercise_4
         public static bool StringMagic(string input1, string input2, TwoStringDelegate method)
         {
             Console.WriteLine($"{ input1} { input2}");
-            Console.WriteLine(method(input1,input2));
+            Console.WriteLine(method(input1, input2));
             Console.WriteLine("----------------------");
             return method(input1, input2);
 
-            
+
         }
-     
+
     }
 }
